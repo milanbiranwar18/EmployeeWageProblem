09ac96aa502f4a4f87db0cc5a06d7b9f3a99a7dc
Milan Biranwar <milanbiranwar06@gmail.com>
2022-07-29 20:21:20 +0530

    Greeting Message

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmployeeWage;
 
-Console.WriteLine("Hello, World!");
-EmployeeAttendance.Employee_Attendance();
+Console.WriteLine("Welcome to Employee Wage Computation Program");
