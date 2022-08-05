91dab6429ce320fffe209e07561f6f57e54c957f
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-05 16:37:51 +0530

    Calculating Employee Wage for multiple companies

--- EmployeeWages.cs
+++ EmployeeWages.cs
@@ -191,5 +191,40 @@ namespace EmployeeWage
 
         }
 
+        
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            int emphrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+
+            {
+                totalWorkingDays++;
+                Random obj = new Random();
+                int num = obj.Next(0, 3);
+                switch (num)
+                {
+                    case IS_FULL_TIME:
+                        emphrs = 12;
+                        break;
+                    case IS_PART_TIME:
+                        emphrs = 8;
+                        break;
+                    default:
+                        emphrs = 0;
+                        break;
+
+                }
+                totalEmpHrs += emphrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + emphrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("total Emp Wage for company : " + company + "is :" + totalEmpWage);
+            return totalEmpWage;
+
+        }
+
     }
 }
