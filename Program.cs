e54d5f0346019fdee390ef8b9bb51495b61c605a
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-09 13:56:39 +0530

    Adding program for EmployeeWage UC13

--- Program.cs
+++ Program.cs
@@ -32,6 +32,7 @@ Console.WriteLine("Welcome to Employee Wage Computation Program");
 EmpWageBuilder empWageBuilder = new EmpWageBuilder();
 empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
 empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+
 empWageBuilder.computeEmpWage();
 
 
